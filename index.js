dcc7e0fcf4aabfe0ad2f791b058e0907045ed13f
Ramil Khabipov <myster@mail.ru>
2024-11-01 18:22:26 +0100

    token

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-
-const octokit = github.getOctokit();
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 function getrepo() {
-    if (process.env.GITHUB_REPOSITORY) {
-        const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-        return { owner, repo };
-    }
-    if (this.payload.repository) {
-        return {
-            owner: this.payload.repository.owner.login,
-            repo: this.payload.repository.name
-        };
-    }
-    throw new Error("required a GITHUB_REPOSITORY environment variable like 'owner/repo'");
+  if (process.env.GITHUB_REPOSITORY) {
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+    return { owner, repo };
+  }
+  if (this.payload.repository) {
+    return {
+      owner: this.payload.repository.owner.login,
+      repo: this.payload.repository.name,
+    };
+  }
+  throw new Error(
+    "required a GITHUB_REPOSITORY environment variable like 'owner/repo'"
+  );
 }
 
-function createComment(owner, repo, issueNumber, body) {
-    const { data: comment } = octokit.rest.issues.createComment({
-        owner: owner,
-        repo: repo,
-        issue_number: issueNumber,
-        body
-    });
-    core.info(`Created comment id '${comment.id}' on issue '${issueNumber}'.`);
-    return comment.id;
+function createComment(octokit, owner, repo, issueNumber, body) {
+  const { data: comment } = octokit.rest.issues.createComment({
+    owner: owner,
+    repo: repo,
+    issue_number: issueNumber,
+    body,
+  });
+  core.info(`Created comment id '${comment.id}' on issue '${issueNumber}'.`);
+  return comment.id;
 }
 
 try {
-  const issueNumber = core.getInput('issue-number');
-  const comment = core.getInput('comment');
+  const issueNumber = core.getInput("issue-number");
+  const comment = core.getInput("comment");
+  const token = core.getInput("token");
+  const octokit = github.getOctokit();
   console.log(`comment: ${comment}!`);
 
   const [owner, repo] = getrepo();
-  const commentId = createComment(owner, repo, issueNumber, comment);
+  const commentId = createComment(octokit, owner, repo, issueNumber, comment);
 
-  core.setOutput('comment-id', commentId);
-
+  core.setOutput("comment-id", commentId);
 } catch (error) {
   core.setFailed(error.message);
 }
